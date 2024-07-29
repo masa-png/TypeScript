9ed9b935d716b3a53c21b18896210cca49f91c8e
masa-png <adidas.org@icloud.com>
2024-07-29 15:13:20 +0900

    first commit

---
+++ src/test.ts
@@ -0,0 +1,87 @@
+export const test = () => {
+  /* const name: string = "Mike";
+  const age: number = 25;
+  const isStudent: boolean = false;
+  const hobbies: string[] = ["Reading", "Coding", "Gaming"];
+  const address: object = {
+    street: "123 Main St",
+    city: "hukuoka",
+    state: "kasuga",
+  };
+  const today: Date = new Date();
+  const none: null = null;
+  const notDefined: undefined = undefined;
+  const random: any = "hello";
+  const gender: "male" | "female" = "male"; //union
+  const value: string | number | null = "hello"; //union
+
+  const reverseArray = (numArray: string[], hasTom?: boolean): string[] => {
+    const reversedArray: string[] = [];
+
+    let index: number = numArray.length - 1;
+    while (index >= 0) {
+      reversedArray.push(numArray[index]);
+      index--;
+    }
+
+    if (hasTom) {
+      reversedArray.push("Tom");
+    }
+    return reversedArray;
+  };
+
+  console.log(reverseArray(["a", "b", "c"], true)); */
+  // const user = {
+  //   userId: 1,
+  //   name: "Mike",
+  //   age: 25,
+  //   email: "mike@gmail.com",
+  //   isActive: true,
+  // };
+  // interface UserProps {
+  //   userId: number;
+  //   name: string;
+  //   age: number;
+  //   email: string;
+  //   isActive: boolean;
+  // }
+  // interface AdminUser extends UserProps {
+  //   role: string;
+  //}
+  // class User {
+  //   userId: number;
+  //   name: string;
+  //   age: number;
+  //   email: string;
+  //   isActive: boolean;
+  //   constructor(
+  //     userId: number,
+  //     name: string,
+  //     age: number,
+  //     email: string,
+  //     isActive: boolean
+  //   ) {
+  //     this.userId = userId;
+  //     this.name = name;
+  //     this.age = age;
+  //     this.email = email;
+  //     this.isActive = isActive;
+  //   }
+  //   private getProfile(): string {
+  //     return `Name: ${this.name}, Age: ${this.age}`;
+  //   }
+  //   static sayHelloWorld(): void {
+  //     console.log("Hello World");
+  //   }
+  // }
+  // const user = new User(1, "Mike", 25, "mike@gmail.com", true);
+  // console.log(user);
+  // User.sayHelloWorld();
+  // const foo = (arg: string): string => {
+  //   return arg;
+  // };
+  // const foo = <T>(arg: T): T => {
+  //   return arg;
+  // };
+  // foo<string>("hello");
+};
